21457c5145710db22d68affaa396a2e9ecd6b748
renfrid <ngolongolo@multics.co.tz>
2023-05-25 15:27:44 +0300

    Minor fix

--- apps/whatsapp/classes.py
+++ apps/whatsapp/classes.py
@@ -2,7 +2,12 @@ import json
 import requests
 from django.http import HttpResponse, JsonResponse
 from decouple import config
+import os
+from pathlib import Path
 import logging
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class WhatsAppWrapper:
     BASE_API_URL = "https://graph.facebook.com/v17.0/"
@@ -122,12 +127,12 @@ class WhatsAppWrapper:
         extension = mime_type.split("/")[1]
 
         #folder name
-        folder_name = "/assets/uploads"
+        folder_name = os.path.join(BASE_DIR, "assets/uploads")
 
         # create a temporary file
         try:
             save_file_here = (
-                f"{folder_name}.{file_name}" if file_name else f"{folder_name}/temp.{extension}"
+                f"{folder_name}/{file_name}" if file_name else f"{folder_name}/temp.{extension}"
             )
             with open(save_file_here, "wb") as f:
                 f.write(content)
