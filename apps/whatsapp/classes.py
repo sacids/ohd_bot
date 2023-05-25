51be4086f6779b5d91a37cbe1e732083103753a5
renfrid <ngolongolo@multics.co.tz>
2023-05-25 13:36:46 +0300

    Minor change

--- apps/whatsapp/classes.py
+++ apps/whatsapp/classes.py
@@ -105,7 +105,7 @@ class WhatsAppWrapper:
         response = requests.get(f"{self.API_URL}/{media_id}", headers=self.headers)
 
         logging.info("media response")
-        logging.info(response.content)
+        logging.info(response.json())
 
         if response.status_code == 200:
             fileURL = response.json()["url"]
