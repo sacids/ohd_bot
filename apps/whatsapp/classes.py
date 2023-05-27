26d76e3ad5a4efe85564e701122919b844184ee8
renfrid <ngolongolo@multics.co.tz>
2023-05-27 16:16:03 +0300

    Minor change

--- apps/whatsapp/classes.py
+++ apps/whatsapp/classes.py
@@ -195,11 +195,11 @@ class WhatsAppWrapper:
             arr_data = []
             for val in arr_trees:
                 data = {
-                    "title": "",
+                    "title": val['title'],
                     "rows": [
                         {
                             "id":val['view_id'],
-                            "title": val['title'],
+                            "title": "",
                             "description": val['description'],           
                         }
                     ]      
