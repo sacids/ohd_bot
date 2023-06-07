3816d3f662cac5a09e426a8d8ce643631082599d
renfrid <ngolongolo@multics.co.tz>
2023-06-07 16:43:12 +0300

    Minor fix

--- assets/js/pages/datatables.init.js
+++ assets/js/pages/datatables.init.js
@@ -9,19 +9,6 @@ $(document).ready(function () {
     buttons: ["copy", "excel", "pdf", "colvis"]
   }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $(".dataTables_length select").addClass("form-select form-select-sm");
 
-
-//   $('#mytable').DataTable({
-//     "paging": false,
-//     "ordering": false,
-//     "info": false,
-//     "bFilter": true,
-//     "pageLength": 100,
-//     "dom": "lrtip"
-// });
-
-// $('#myCustomSearchBox').keyup(function() {
-//     dTable.search($(this).val()).draw(); // this  is for customized searchbox with datatable search feature.
-// });
 });
 /******/ })()
 ;
