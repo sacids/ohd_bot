4d82e74819951cc3519ecb46713bdfdfe8229269
renfrid <ngolongolo@multics.co.tz>
2023-06-12 10:33:04 +0300

    Minor fix

--- apps/whatsapp/views.py
+++ apps/whatsapp/views.py
@@ -243,7 +243,7 @@ def process_threads(**kwargs):
         thread_session = ThreadSession.objects.filter(phone=from_number, active=0)
 
         if thread_session.count() > 0:
-            if key.upper() == "LAINA" or key.upper() == "HUDUMA" or key == 0:
+            if key.upper() == "LAINA" or key.upper() == "HUDUMA" or key == "0":
                 """update all menu sessions"""
                 ThreadSession.objects.filter(phone=from_number).update(active=1)
 
