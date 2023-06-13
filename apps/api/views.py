daa6aad3eefcc6a8af23bdbbe7087ab24364239e
renfrid <ngolongolo@multics.co.tz>
2023-06-13 14:57:11 +0300

    Minor change

--- apps/api/views.py
+++ apps/api/views.py
@@ -8,7 +8,9 @@ from apps.thread.classes import ThreadWrapper
 
 def insurance_information(request):
     """Sample API for pull data """
-    uuid = request.GET['uuid']
+    uuid = request.POST['uuid']
+    logging.info("params => ")
+    logging.info(uuid)
 
     wrapper = ThreadWrapper()
     request = wrapper.process_data(uuid= uuid)
@@ -18,70 +20,70 @@ def insurance_information(request):
     message = "*_Hakiki Taarifa za madai ya BIMA_* \n\n" \
     
     if 'Taarifa' in response['arr_data']:
-        message += "*Unatoa Taarifa kama:* " + response['arr_data']['Taarifa'] + " \n"
+        message += "*Unatoa Taarifa kama:* " + response['arr_data']['Taarifa'] + " \r\n"
         
     if 'Phone' in response['arr_data']:
-        message += "*Namba ya Simu:* " + response['arr_data']['Phone'] + " \n" 
+        message += "*Namba ya Simu:* " + response['arr_data']['Phone'] + " \r\n" 
 
     if 'Vehicle_Number' in response['arr_data']:
-        message += "*Namba ya usajili wa chombo:* " + response['arr_data']['Vehicle_Number'] + " \n" 
+        message += "*Namba ya usajili wa chombo:* " + response['arr_data']['Vehicle_Number'] + " \r\n" 
 
     if 'Insurance_Type' in response['arr_data']:
-        message += "*Aina ya Madai:* " + response['arr_data']['Insurance_Type'] + " \n"  
+        message += "*Aina ya Madai:* " + response['arr_data']['Insurance_Type'] + " \r\n"  
 
     if 'Accident_Date' in response['arr_data']:
-        message += "*Tarehe Ya Ajali:* " + response['arr_data']['Accident_Date'] + " \n"  
+        message += "*Tarehe Ya Ajali:* " + response['arr_data']['Accident_Date'] + " \r\n"  
 
     if 'Theft_Date' in response['arr_data']:
-        message += "*Tarehe Ya Wizi:* " + response['arr_data']['Theft_Date'] + " \n"  
+        message += "*Tarehe Ya Wizi:* " + response['arr_data']['Theft_Date'] + " \r\n"  
 
     if 'Accident_Time' in response['arr_data']:
-        message += "*Muda wa Ajali:* " + response['arr_data']['Accident_Time'] + " \n"  
+        message += "*Muda wa Ajali:* " + response['arr_data']['Accident_Time'] + " \r\n"  
 
     if 'Theft_Time' in response['arr_data']:
-        message += "*Muda wa Wizi:* " + response['arr_data']['Theft_Time'] + " \n"  
+        message += "*Muda wa Wizi:* " + response['arr_data']['Theft_Time'] + " \r\n"  
 
     if 'Accident_Location' in response['arr_data']:
-        message += "*Sehemu/Mahali:* " + response['arr_data']['Accident_Location'] + " \n"  
+        message += "*Sehemu/Mahali:* " + response['arr_data']['Accident_Location'] + " \r\n"  
 
     if 'Theft_Location' in response['arr_data']:
-        message += "*Sehemu/Mahali:* " + response['arr_data']['Theft_Location'] + " \n" 
+        message += "*Sehemu/Mahali:* " + response['arr_data']['Theft_Location'] + " \r\n" 
 
     if 'Driver_Licence' in response['arr_data']:
-        message += "*Leseni ya Dereva:* " + response['arr_data']['Driver_Licence'] + " \n"   
+        message += "*Leseni ya Dereva:* " + response['arr_data']['Driver_Licence'] + " \r\n"   
 
     if 'Police_Station' in response['arr_data']:
-        message += "*Jina la Kituo Cha Polisi:* " + response['arr_data']['Police_Station'] + " \n" 
+        message += "*Jina la Kituo Cha Polisi:* " + response['arr_data']['Police_Station'] + " \r\n" 
         
     if 'Police_RB' in response['arr_data']:
-        message += "*Kumbukumbu Namba(RB):* " + response['arr_data']['Police_RB'] + " \n" 
+        message += "*Kumbukumbu Namba(RB):* " + response['arr_data']['Police_RB'] + " \r\n" 
     
     if 'Accident_Reason' in response['arr_data']:
-        message += "*Sababu ya ajali:* " + response['arr_data']['Accident_Reason'] + " \n" 
+        message += "*Sababu ya ajali:* " + response['arr_data']['Accident_Reason'] + " \r\n" 
 
     if 'Vehicle_Damage' in response['arr_data']:
-        message += "*Madhara kwenye chombo:* " + response['arr_data']['Vehicle_Damage'] + " \n"      
+        message += "*Madhara kwenye chombo:* " + response['arr_data']['Vehicle_Damage'] + " \r\n"      
 
     if 'Other_Vehicle_Damage' in response['arr_data']:
-        message += "*Uharibifu wa vyombo vingine:* " + response['arr_data']['Other_Vehicle_Damage'] + " \n" 
+        message += "*Uharibifu wa vyombo vingine:* " + response['arr_data']['Other_Vehicle_Damage'] + " \r\n" 
 
     if 'Other_Vehicle_Damage_Registration' in response['arr_data']:
-        message += "*Namba za usajili wa vyombo vingine :* " + response['arr_data']['Other_Vehicle_Damage_Registration'] + " \n" 
+        message += "*Namba za usajili wa vyombo vingine :* " + response['arr_data']['Other_Vehicle_Damage_Registration'] + " \r\n" 
 
     if 'Property_Damage' in response['arr_data']:
-        message += "*Hasara kwa mali zingine:* " + response['arr_data']['Property_Damage'] + " \n" 
+        message += "*Hasara kwa mali zingine:* " + response['arr_data']['Property_Damage'] + " \r\n" 
 
     if 'Property_Theft' in response['arr_data']:
-        message += "*Je vitu vimeibiwa?:* " + response['arr_data']['Property_Theft'] + " \n" 
+        message += "*Je vitu vimeibiwa?:* " + response['arr_data']['Property_Theft'] + " \r\n" 
 
     if 'Property_Theft_Info' in response['arr_data']:
-        message += "*Vitu vilivyoibiwa:* " + response['arr_data']['Property_Theft_Info'] + " \n" 
+        message += "*Vitu vilivyoibiwa:* " + response['arr_data']['Property_Theft_Info'] + " \r\n" 
 
     if 'People_Damage' in response['arr_data']:
-        message += "*Majeruhi kwa watu:* " + response['arr_data']['People_Damage'] + " \n" 
+        message += "*Majeruhi kwa watu:* " + response['arr_data']['People_Damage'] + " \r\n" 
 
     if 'Number_of_Casualties' in response['arr_data']:
-        message += "*Idadi ya Majeruhi:* " + response['arr_data']['Number_of_Casualties'] + " \n\n\n\n" 
+        message += "*Idadi ya Majeruhi:* " + response['arr_data']['Number_of_Casualties'] + " \r\n\n\n\n" 
 
     message += "*Declaration (Tamko):* Mimi/Sisi nina/tunatoa tamko ya kwamba maelezo yaliyotolewa hapo juu pamoja na muhtasari wake ni ya ukweli na yamekamilika. Kama sivyo ni/tutawajibika kwa yatakayojiri."
      
--- apps/thread/classes.py
+++ apps/thread/classes.py
@@ -519,6 +519,7 @@ class ThreadWrapper:
 
         arr_params = {}
         arr_params['msisdn'] = msisdn
+        arr_params['uuid'] = uuid
 
         if payload is not None:
             arr_payload = payload.split(',')
