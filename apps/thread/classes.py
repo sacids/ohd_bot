86a16c209acb1c6e631eeea9ebf943a895d0698d
renfrid <ngolongolo@multics.co.tz>
2023-06-15 23:20:36 +0300

    Minor fix

--- apps/thread/classes.py
+++ apps/thread/classes.py
@@ -439,6 +439,7 @@ class ThreadWrapper:
         elif thread.action is not None and thread.action == "PUSH":
             #build params
             arr_params = self.build_payload(payload=thread.payload, msisdn=phone, uuid=uuid)
+            logging.info(arr_params)
         
             #call pull api
             request = requests.post(thread.action_url, data=arr_params)
