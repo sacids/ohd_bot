38e9696c308799be6858a3674c0b482b90e57ba2
renfrid <renfridfrancis@gmail.com>
2023-06-23 10:23:05 +0300

    Minor fix

--- apps/api/views.py
+++ apps/api/views.py
@@ -23,23 +23,23 @@ def rumor_information(request):
             #construct message
             message = "*Hakiki Taarifa* \n\n" \
             
-            if 'Text' in response['arr_data']:
-                message += "*Maelezo ya Tukio:* " + response['arr_data']['Text'] + " \r\n"
+            if 'text' in response['arr_data']:
+                message += "*Maelezo ya Tukio:* " + response['arr_data']['text'] + " \r\n"
                 
-            if 'Village' in response['arr_data']:
-                message += "*Kijiji/Mtaa:* " + response['arr_data']['Village'] + " \r\n" 
+            if 'village' in response['arr_data']:
+                message += "*Kijiji/Mtaa:* " + response['arr_data']['village'] + " \r\n" 
 
-            if 'Ward' in response['arr_data']:
-                message += "*Kata:* " + response['arr_data']['Ward'] + " \r\n" 
+            if 'ward' in response['arr_data']:
+                message += "*Kata:* " + response['arr_data']['ward'] + " \r\n" 
 
-            if 'Rumor_Date' in response['arr_data']:
-                message += "*Tarehe ya Tukio:* " + response['arr_data']['Rumor_Date'] + " \r\n"  
+            if 'date' in response['arr_data']:
+                message += "*Tarehe ya Tukio:* " + response['arr_data']['date'] + " \r\n"  
 
-            if 'Street' in response['arr_data']:
-                message += "*Jina Maarufu la Eneo:* " + response['arr_data']['Street'] + " \r\n"  
+            if 'street' in response['arr_data']:
+                message += "*Jina Maarufu la Eneo:* " + response['arr_data']['street'] + " \r\n"  
 
-            if 'Location' in response['arr_data']:
-                message += "*Mahali(GPS):* " + response['arr_data']['Location'] + " \r\n"  
+            if 'location' in response['arr_data']:
+                message += "*Mahali(GPS):* " + response['arr_data']['location'] + " \r\n"  
 
 
             message += f"\r\n\n*Tamko: Ninathibitisha maelezo ni ya ukweli na ni sahihi."
